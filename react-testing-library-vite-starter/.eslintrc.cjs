c39c97761a013679be5e89376bad2090e19dc985
ch <mkmsacc@outlook.com>
2024-01-01 21:12:49 +0700

    Update ESLint configuration with new rules and plugins
    The ESLint configuration file '.eslintrc.cjs' is updated to include new packages like 'vitest' and additional react-related plugins. Parser options and settings are also enhanced for better code analysis. This change makes our linter more comprehensive, ensuring that our code complies with most recent best practices.

--- react-testing-library-vite-starter/.eslintrc.cjs
+++ react-testing-library-vite-starter/.eslintrc.cjs
@@ -1,3 +1,4 @@
+const vitest = require("eslint-plugin-vitest");
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
@@ -5,14 +6,22 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:vitest/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {ecmaVersion: 'latest', sourceType: 'module'},
+  settings: {react: {version: '18.2'}},
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'vitest'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
+  globals: {
+    ...vitest.environments.env.globals,
+  }
 }
