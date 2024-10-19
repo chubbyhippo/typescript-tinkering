efc2280de52d307fa5bb5066631b9d33858993cc
chubbyhippo <mkmsacc@outlook.com>
2024-10-19 18:56:24 +0700

    Remove Vite starter files
    Deleted various configuration, source, and dependency files associated with the React Testing Library Vite starter project. This includes both user and test-related files, reducing project size and complexity.

--- react-testing-library-vite-starter/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import App from "./App";
-import {render, screen} from "@testing-library/react";
-
-test("should contains correct heading", () => {
-    render(<App/>);
-    const headingElement = screen.getByText(/react/i);
-    expect(headingElement).toBeInTheDocument();
-
-});
--- react-testing-library-vite-starter/src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-    return (
-        <div>
-            <h1>React Testing Library</h1>
-        </div>
-    );
-}
-
-export default App;
