27526b567cfd078682b4e8875eef9450e14b8a1b
ch <mkmsacc@outlook.com>
2024-10-22 12:52:44 +0700

    Update App component to display a list of stories
    Added a static list of stories and rendered them with a map function. Changed the header text to 'My Hacker Stories' and introduced a basic search input field.

--- road-to-react/src/App.tsx
+++ road-to-react/src/App.tsx
@@ -1,7 +1,36 @@
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
+
+      <hr />
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>{item.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
