b96ee6cce2b0f0e03bf79f87f66c094f4a9dcae6
chubbyhippo <mkmsacc@outlook.com>
2024-10-23 10:44:14 +0700

    Enhance list items with additional details
    Updated the list items to include links to the titles, along with author names, comment counts, and point scores. This improves the usability and richness of the displayed information.

--- road-to-react/src/App.tsx
+++ road-to-react/src/App.tsx
@@ -28,7 +28,14 @@ function App() {
       <hr />
       <ul>
         {list.map((item) => (
-          <li key={item.objectID}>{item.title}</li>
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
         ))}
       </ul>
     </div>
