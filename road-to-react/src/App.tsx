045f63870cb714ac019a3d33932fc8351fd5e725
ch <mkmsacc@outlook.com>
2024-10-24 12:15:35 +0700

    Refactor list rendering into a separate List component.
    This change extracts the list rendering logic from App component into a new List component. It improves readability and better organizes the code structure, making it easier to maintain and scale.

--- road-to-react/src/App.tsx
+++ road-to-react/src/App.tsx
@@ -25,19 +25,24 @@ const App = () => (
     <input type="text" id="search" />
 
     <hr />
-    <ul>
-      {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span>{item.author}</span>
-          <span>{item.num_comments}</span>
-          <span>{item.points}</span>
-        </li>
-      ))}
-    </ul>
+
+    <List />
   </div>
+);
+
+const List = () => (
+  <ul>
+    {list.map((item) => (
+      <li key={item.objectID}>
+        <span>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span>{item.author}</span>
+        <span>{item.num_comments}</span>
+        <span>{item.points}</span>
+      </li>
+    ))}
+  </ul>
 );
 
 export default App;
