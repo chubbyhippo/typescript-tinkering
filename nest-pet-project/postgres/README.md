bb783116af1a42c4e08a08e10d6f97c53b02785a
ch <mkmsacc@outlook.com>
2025-02-08 17:19:02 +0700

    Setup database

---
+++ nest-pet-project/postgres/README.md
@@ -0,0 +1,4 @@
+# docker-compose-postgresql
+```shell
+git clone --depth=1 https://github.com/chubbyhippo/docker-compose-postgresql.git postgres && rm -rf postgres/.git
+```
---
+++ nest-pet-project/src/database/database.providers.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATA_SOURCE',
+    useFactory: async () => {
+      const dataSource = new DataSource({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'admin',
+        password: 'admin',
+        database: 'mydatabase',
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+        synchronize: true, // Set to false in production
+      });
+
+      return dataSource.initialize();
+    },
+  },
+];
