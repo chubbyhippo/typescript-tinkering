84d93d86ec0ba093406422bbb8bb951008edcf7e
ch <mkmsacc@outlook.com>
2025-02-09 16:38:31 +0700

    Add not require field for swagger

--- nest-pet-project/src/pets/pets.controller.ts
+++ nest-pet-project/src/pets/pets.controller.ts
@@ -5,6 +5,7 @@ import { CreatePetDto } from './dto/create-pet-dto';
 import { GetAllPetsQuery } from './queries/get-all-pets.query';
 import { GetAllPetsPaginationQuery } from './queries/get-all-pets-pagination.query';
 import { GetAllPetsRawSqlQuery } from './queries/get-all-pets-raw-sql.query';
+import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('pets')
 export class PetsController {
@@ -25,6 +26,8 @@ export class PetsController {
   }
 
   @Get('pagination')
+  @ApiQuery({name: 'page', required: false})
+  @ApiQuery({name: 'limit', required: false})
   async getAllPetsPagination(
     @Query('page') page: number = 1,
     @Query('limit') limit: number = 10,
