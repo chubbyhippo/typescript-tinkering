cbf2265a3d7547af0c20aa5bd1627e620ae34b3b
chubbyhippo <mkmsacc@outlook.com>
2025-02-10 20:59:59 +0700

    Import relative

--- nest-pet-project/src/pets/pets.module.ts
+++ nest-pet-project/src/pets/pets.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { PetsController } from './pets.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PetEntity } from './pet.entity';
-import { CategoryEntity } from 'src/categories/category.entity';
 import { CqrsModule } from '@nestjs/cqrs';
 import { CreatePetHandler } from './commands/create-pet.handler';
 import { GetAllPetsHandler } from './queries/get-all-pets.handler';
 import { GetAllPetsPaginationHandler } from './queries/get-all-pets-pagination.handler';
 import { GetAllPetsRawSqlHandler } from './queries/get-all-pets-raw-sql.handler';
+import { CategoryEntity } from '../categories/category.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([PetEntity, CategoryEntity]), CqrsModule],
