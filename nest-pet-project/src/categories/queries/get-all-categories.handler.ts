5f24ff5e9e6d7ddb182cdd9fab58aa31634450fd
chubbyhippo <mkmsacc@outlook.com>
2025-02-10 21:06:49 +0700

    Remove unused var

--- nest-pet-project/src/categories/queries/get-all-categories.handler.ts
+++ nest-pet-project/src/categories/queries/get-all-categories.handler.ts
@@ -1,18 +1,19 @@
-import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
-import { GetAllCategoriesQuery } from "./get-all-categories.query";
-import { InjectRepository } from "@nestjs/typeorm";
-import { CategoryEntity } from "../category.entity";
-import { Repository } from "typeorm";
+import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
+import { GetAllCategoriesQuery } from './get-all-categories.query';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from '../category.entity';
+import { Repository } from 'typeorm';
 
 @QueryHandler(GetAllCategoriesQuery)
-export class GetAllCategoriesHandler implements IQueryHandler<GetAllCategoriesQuery>{
-    constructor(
-        @InjectRepository(CategoryEntity)
-        private readonly categoryRepository: Repository<CategoryEntity>,
-    ){}
+export class GetAllCategoriesHandler
+  implements IQueryHandler<GetAllCategoriesQuery>
+{
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private readonly categoryRepository: Repository<CategoryEntity>,
+  ) {}
 
-    execute(_query: GetAllCategoriesQuery): Promise<CategoryEntity[]> {
-        return this.categoryRepository.find({relations: ['pets']});
-    }
-
+  execute(): Promise<CategoryEntity[]> {
+    return this.categoryRepository.find({ relations: ['pets'] });
+  }
 }
